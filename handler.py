aa493656f6aec33ce0e82cc816521a9c71bf699c
Trey Brooks <monroemasseybrooks@gmail.com>
2019-03-29 15:40:12 -0500

    updating the request ride function and adding tester functions

--- handler.py
+++ handler.py
@@ -1,5 +1,18 @@
 import json
+import decimal
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+
+table_name = 'taxiTable'
+
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return str(o)
+        return super(DecimalEncoder, self).default(o)
 
 
 def list_resources(event, context):
@@ -17,3 +30,31 @@ def list_resources(event, context):
     }
 
     return response
+
+def request_ride(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+
+    print(event['queryStringParameters'])
+
+    fe =   Attr('passenger_count').eq(int(event['queryStringParameters']['passenger_count'])) \
+         & Attr('PULocationID').eq(int(event['queryStringParameters']['pickup_location'])) \
+         & Attr('DOLocationID').eq(int(event['queryStringParameters']['dropoff_location'])) \
+         & Attr('tpep_pickup_datetime').eq(event['queryStringParameters']['pickup_datetime'])
+
+    scan_response = table.scan(FilterExpression=fe)
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(scan_response['Items'], cls=DecimalEncoder)
+    }
+
+    return response
+
+# def list_availible_vendors(event, context):
+#     response = {
+#         "statusCode": 200,
+#         "body": json.dumps(body)
+#     }
+
+#     return response
---
+++ helper_fxns.py
@@ -0,0 +1,52 @@
+import boto3
+import uuid
+import json
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
+
+# Get the service resource.
+dynamodb_resource = boto3.resource('dynamodb', region_name = 'us-east-1')
+dynamodb_client = boto3.client('dynamodb', region_name = 'us-east-1')
+table_name = 'taxiTable'
+
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return str(o)
+        return super(DecimalEncoder, self).default(o)
+
+
+def request_ride(event_dict):
+    # client = boto3.client('dynamodb')
+    table = dynamodb_resource.Table(table_name)
+
+    fe = Attr('passenger_count').eq(event_dict['passenger_count']) & \
+         Attr('PULocationID').eq(event_dict['pickup_location']) & \
+         Attr('DOLocationID').eq(event_dict['dropoff_location'])
+    # pe = "#yr, title, info.rating"
+    # Expression Attribute Names for Projection Expression only.
+    ean = { "#yr": "year", }
+    esk = None
+
+    scan_response = table.scan(
+        FilterExpression=fe,
+        # ProjectionExpression=pe,
+        # ExpressionAttributeNames=ean
+    )
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(scan_response['Items'], cls=DecimalEncoder)
+    }
+
+    return response
+
+
+response = request_ride({
+    'passenger_count': 2,
+    'pickup_location': 249,
+    'dropoff_location': 246
+    })
+print(response)
